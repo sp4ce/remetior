fefbb651b89524f3ec7a180550d2cb2ada54ba72
Baptiste Pernet <bapt@sp4ce.net>
2012-02-27 16:52:00 +0100

    Put the file uploader, remove some useless stuff

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -1,6 +1,5 @@
-// Apache 2.0 J Chris Anderson 2011
 $(function() {   
-    // friendly helper http://tinyurl.com/6aow6yn
+    // Friendly helper http://tinyurl.com/6aow6yn to serialize form to json with jQuery.
     $.fn.serializeObject = function() {
         var o = {};
         var a = this.serializeArray();
@@ -17,43 +16,55 @@ $(function() {
         return o;
     };
 
+    // Create the database object to read and connect to CouchDB
     var path = unescape(document.location.pathname).split('/'),
         design = path[3],
         db = $.couch.db(path[1]);
-    function drawItems() {
-        db.view(design + "/recent-items", {
-            descending : "true",
-            limit : 50,
-            update_seq : true,
-            success : function(data) {
-                setupChanges(data.update_seq);
-                var them = $.mustache($("#recent-messages").html(), {
-                    items : data.rows.map(function(r) {return r.value;})
-                });
-                $("#content").html(them);
-            }
-        });
-    };
-    drawItems();
-    var changesRunning = false;
-    function setupChanges(since) {
-        if (!changesRunning) {
-            var changeHandler = db.changes(since);
-            changesRunning = true;
-            changeHandler.onChange(drawItems);
-        }
-    }
-    $.couchProfile.templates.profileReady = $("#new-message").html();
+    
+  //  function drawItems() {
+  //      db.view(design + "/recent-items", {
+  //          descending : "true",
+  //          limit : 50,
+  //          update_seq : true,
+  //          success : function(data) {
+  //              setupChanges(data.update_seq);
+  //              var them = $.mustache($("#recent-messages").html(), {
+  //                  items : data.rows.map(function(r) {return r.value;})
+  //              });
+  //              $("#content").html(them);
+  //          }
+  //      });
+  //  };
+  //  drawItems();
+  //  var changesRunning = false;
+  //  function setupChanges(since) {
+  //      if (!changesRunning) {
+  //          var changeHandler = db.changes(since);
+  //          changesRunning = true;
+  //          changeHandler.onChange(drawItems);
+  //      }
+  //  }
+    
+    // Evently binding
+//    $.couch.app(function(app) {
+//        $("#account").evently("account", app);
+//        $("#profile").evently("profile", app);
+//        $.evently.connect("#account", "#profile", ["loggedIn", "loggedOut"]);
+//        //$.couchProfile.templates.profileReady = ($('<div></div>').evently("uploader", app)).html();
+//    });
+
+    // After the user has logged in, display the uploader.
+    $.couchProfile.templates.profileReady = $("#uploader").html();
     $("#account").couchLogin({
         loggedIn : function(r) {
             $("#profile").couchProfile(r, {
                 profileReady : function(profile) {
-                    $("#create-message").submit(function(e){
+                    $("#upload-sheet").submit(function(e){
                         e.preventDefault();
-                        var form = this, doc = $(form).serializeObject();
-                        doc.created_at = new Date();
-                        doc.profile = profile;
-                        db.saveDoc(doc, {success : function() {form.reset();}});
+                  //      var form = this, doc = $(form).serializeObject();
+                  //      doc.created_at = new Date();
+                  //      doc.profile = profile;
+                  //      db.saveDoc(doc, {success : function() {form.reset();}});
                         return false;
                     }).find("input").focus();
                 }
