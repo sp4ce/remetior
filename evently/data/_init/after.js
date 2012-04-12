6ec4362ac01a89f09e2f03ee796b7a4d9005f903
Baptiste Pernet <bapt@sp4ce.net>
2012-04-12 12:24:46 +0200

    Fix about the month (include the year...) and display table of data.

--- evently/data/_init/after.js
+++ evently/data/_init/after.js
@@ -1,5 +1,10 @@
 function(data) {
      for (var i in data.charts) {
+         // Draw table.
+         var table = new google.visualization.Table(document.getElementById('table_' + i));
+         table.draw(data.charts[i]);
+
+         // Draw chart.
          var chart = new google.visualization.LineChart(document.getElementById('chart_' + i));
          chart.draw(data.charts[i], data.options[i]);
      }
